1d0e1b2845285a2ee43184062d16b8cf15772782
chsu5358 <bulucyhbulu@gmail.com>
2017-10-06 17:37:53 -0400

    edit bib-list.js

--- js/bib-list.js
+++ js/bib-list.js
@@ -2525,20 +2525,32 @@ var bibtexify = (function($) {
         // adds links to the PDF or url of the item
         links: function(entryData) {
             var itemStr = '';
+            // if (entryData.url && entryData.url.match(/.*\.pdf/)) {
+            //     itemStr += ' (<a title="PDF-version of this article" href="' +
+            //                 entryData.url + '">pdf<\/a>)';
+            // } else if (entryData.url) {
+            //     itemStr += ' (<a title="This article online" href="' + entryData.url +
+            //                 '">link<\/a>)';
+            // }
+            /* CY, 2017: edit style */
             if (entryData.url && entryData.url.match(/.*\.pdf/)) {
-                itemStr += ' (<a title="PDF-version of this article" href="' +
-                            entryData.url + '">pdf<\/a>)';
+                itemStr += ' [<a title="PDF-version of this article" href="' +
+                            entryData.url + '">pdf<\/a>]';
             } else if (entryData.url) {
-                itemStr += ' (<a title="This article online" href="' + entryData.url +
-                            '">link<\/a>)';
+                itemStr += ' [<a title="This article online" href="' + entryData.url +
+                            '">link<\/a>]';
             }
             return itemStr;
         },
         // adds the bibtex link and the opening div with bibtex content
         bibtex: function(entryData) {
             var itemStr = '';
-            itemStr += ' (<a title="This article as BibTeX" href="#" class="biblink">' +
-                        'bib</a>)<div class="bibinfo hidden">';
+            // itemStr += ' (<a title="This article as BibTeX" href="#" class="biblink">' +
+            //             'bib</a>)<div class="bibinfo hidden">';
+            /* CY, 2017: edit style */
+            itemStr += ' [<a title="This article as BibTeX" href="#" class="biblink">' +
+                        'bib</a>]<div class="bibinfo hidden">';
+
             itemStr += '<a href="#" class="bibclose" title="Close">x</a><pre>';
             itemStr += '@' + entryData.entryType + "{" + entryData.cite + ",\n";
             $.each(entryData, function(key, value) {
