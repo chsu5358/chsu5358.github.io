586f41f98516a48fc0a5e5da0f370dca946ca911
chsu5358 <bulucyhbulu@gmail.com>
2017-10-20 22:10:13 -0400

    edit smoothscroll.js for ppl

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -32,4 +32,4 @@ a=b.extend({},c.defaults,a);return a.lazy?this.bind(a.event,function(d){var c=b(
 // Initialize all .smoothScroll links
 // jQuery(function($){ $.localScroll({filter:'.smoothScroll'}); });
 /* CY, 2017: Edit target location offset */
-jQuery(function($){ $.localScroll({filter:'.smoothScroll', offset:{top:-100}}); });
+jQuery(function($){ $.localScroll({filter:'.smoothScroll', offset:{top:-60}}); });
